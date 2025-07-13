76164ee88e52d6176f418c4b425c12b381c6495b
odutrajs <dutrainformatica5@gmail.com>
2025-07-13 20:21:12 -0300

    feat: add testeCafe

---
+++ tests/news.test.ts
@@ -0,0 +1,11 @@
+import { Selector } from "testcafe";
+
+fixture("Teste noticias").page("http://localhost:5173/news/");
+
+test("Verifica se a página de notícias exibe o botão de criar", async (t) => {
+  const createButton = Selector("button").withText("Criar nova notícia");
+
+  await t
+    .expect(createButton.exists)
+    .ok("Botão de criar nova notícia deve existir");
+});
